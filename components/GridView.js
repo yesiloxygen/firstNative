c8f8f5050a4e1509a605f41e501b0ab453fb92dc
yesiloxygen <new_guns@hotmail.com>
2018-07-07 00:20:06 +0300

    button test added;

--- components/GridView.js
+++ components/GridView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet , FlatList, Dimensions, Image} from 'react-native';
 
+import Button from './Button';
+
 //TODO_BAHAR num columsu propstan alıp nasıl yapabilirim
 export class GridView extends Component {
     data = [
@@ -23,7 +25,24 @@ export class GridView extends Component {
           flex: 1,
           margin: 3,
           backgroundColor: 'lightblue',
-        }
+        },
+        subContainerStyle1: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        borderColor: '#ddd',
+        position: 'relative'
+      },
+      subContainerStyle2: {
+        borderBottomWidth: 1,
+        padding: 5,
+        marginTop: 30,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        borderColor: '#ddd',
+        position: 'relative'
+      },
       });
 
 
@@ -34,13 +53,17 @@ export class GridView extends Component {
         <FlatList
           data={this.data}
           renderItem={({item}) => (
-            <View style={this.styles.itemContainer}>
-              <Text style={this.styles.item}>{item.value}</Text>
-              <Image style={this.styles.itemContainer}
-              source = {{ uri: 
-                 'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png' }}
-                
-                 />
+            <View style={this.styles.itemContainer}>             
+               <View style={this.styles.subContainerStyle1}>
+                <Image style={this.styles.itemContainer}
+                source = {{ uri: 
+                  'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png' }}
+                  
+                  />
+                </View>
+                <View style={this.styles.subContainerStyle2}>
+                <Button onPress={() => console.log("button clicked")}> Button </Button>
+                </View>
             </View>
           )}
           keyExtractor={item => item.id}
